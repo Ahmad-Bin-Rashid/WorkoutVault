08b3cd66919bff191e4da8315cc9ac91997cba46
Ahmad-Bin-Rashid <ahmadbinrashid92@gmail.com>
2025-07-06 11:40:26 +0500

    refactor: rename Backend to backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken')
 
 // creating JWT token
 const createToken = (_id) => {
-    return jwt.sign({ _id }, process.env.SECRET, { expiresIn: '3d' })
+    return jwt.sign({ _id }, process.env.JWT_SECRET, { expiresIn: '3d' })
 }
 
 
